6f2866e75ea2e99ee37e763c59ad206d804b4b78
Michelangelo Calori <70917863+mikizdt@users.noreply.github.com>
2024-08-13 21:44:27 -0400

    Last changes in HTML and CSS before starting the logic side of the app

---
+++ index.js
@@ -0,0 +1,5 @@
+//Onclick the game buttons have to generate a random choice.
+//Target the selected player choice and compare to computer chioce
+//Program and calculate result
+//Display game result
+//Reset button to reset the game
